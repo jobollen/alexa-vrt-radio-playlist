16a2a1ef45d2566f7e4659cba42a424b447186f1
jobollen <bollen.jo@gmail.com>
2017-06-04 16:14:26 +0200

    Updated src/index, refactored httpsGetVrtService() function.

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ const APP_ID = '';  // TODO replace with your app ID (OPTIONAL).';
 // 1. Text strings =====================================================================================================
 //    Modify these strings and messages to change the behavior of your Lambda function
 
-const vrtRadioChannels = {
+const vrtRadioChannel = {
   'mnm'   : {
     'code': 55,
     'speech': 'M and M',
@@ -45,103 +45,32 @@ const vrtRadioChannels = {
   },
 };
 
-const vrtServicePlaylistOnair = {
-  host: 'services.vrt.be',
-  path: '/playlist/onair',
-  //port: '80',
-  method: 'GET',
-  headers: {
-    'Accept': 'application/vnd.playlist.vrt.be.noa_1.0+json',
+const vrtService = {
+  playlistOnair: {
+    host: 'services.vrt.be',
+    path: '/playlist/onair',
+    //port: '80',
+    method: 'GET',
+    headers: {
+      'Accept': 'application/vnd.playlist.vrt.be.noa_1.0+json',
+    }
   }
 };
 
 const languageStrings = {
   'en': {
     translation: {
-      FACTS: [
-        'A year on Mercury is just 88 days long.',
-        'Despite being farther from the Sun, Venus experiences higher temperatures than Mercury.',
-        'Venus rotates counter-clockwise, possibly because of a collision in the past with an asteroid.',
-        'On Mars, the Sun appears about half the size as it does on Earth.',
-        'Earth is the only planet not named after a god.',
-        'Jupiter has the shortest day of all the planets.',
-        'The Milky Way galaxy will collide with the Andromeda Galaxy in about 5 billion years.',
-        'The Sun contains 99.86% of the mass in the Solar System.',
-        'The Sun is an almost perfect sphere.',
-        'A total solar eclipse can happen once every 1 to 2 years. This makes them a rare event.',
-        'Saturn radiates two and a half times more energy into space than it receives from the sun.',
-        'The temperature inside the Sun can reach 15 million degrees Celsius.',
-        'The Moon is moving approximately 3.8 cm away from our planet every year.',
-      ],
-      SKILL_NAME: 'Space Facts',
-      GET_FACT_MESSAGE: "Here's your fact: ",
-      HELP_MESSAGE: 'You can say tell me a space fact, or, you can say exit... What can I help you with?',
+      HELP_MESSAGE: 'You can say "tell me the latest song on studio brussels", or, you can say exit... What can I help you with?',
       HELP_REPROMPT: 'What can I help you with?',
       STOP_MESSAGE: 'Goodbye!',
     },
   },
   'en-US': {
     translation: {
-      FACTS: [
-        'A year on Mercury is just 88 days long.',
-        'Despite being farther from the Sun, Venus experiences higher temperatures than Mercury.',
-        'Venus rotates counter-clockwise, possibly because of a collision in the past with an asteroid.',
-        'On Mars, the Sun appears about half the size as it does on Earth.',
-        'Earth is the only planet not named after a god.',
-        'Jupiter has the shortest day of all the planets.',
-        'The Milky Way galaxy will collide with the Andromeda Galaxy in about 5 billion years.',
-        'The Sun contains 99.86% of the mass in the Solar System.',
-        'The Sun is an almost perfect sphere.',
-        'A total solar eclipse can happen once every 1 to 2 years. This makes them a rare event.',
-        'Saturn radiates two and a half times more energy into space than it receives from the sun.',
-        'The temperature inside the Sun can reach 15 million degrees Celsius.',
-        'The Moon is moving approximately 3.8 cm away from our planet every year.',
-      ],
-      SKILL_NAME: 'American Space Facts',
     },
   },
   'en-GB': {
     translation: {
-      FACTS: [
-        'A year on Mercury is just 88 days long.',
-        'Despite being farther from the Sun, Venus experiences higher temperatures than Mercury.',
-        'Venus rotates anti-clockwise, possibly because of a collision in the past with an asteroid.',
-        'On Mars, the Sun appears about half the size as it does on Earth.',
-        'Earth is the only planet not named after a god.',
-        'Jupiter has the shortest day of all the planets.',
-        'The Milky Way galaxy will collide with the Andromeda Galaxy in about 5 billion years.',
-        'The Sun contains 99.86% of the mass in the Solar System.',
-        'The Sun is an almost perfect sphere.',
-        'A total solar eclipse can happen once every 1 to 2 years. This makes them a rare event.',
-        'Saturn radiates two and a half times more energy into space than it receives from the sun.',
-        'The temperature inside the Sun can reach 15 million degrees Celsius.',
-        'The Moon is moving approximately 3.8 cm away from our planet every year.',
-      ],
-      SKILL_NAME: 'British Space Facts',
-    },
-  },
-  'de': {
-    translation: {
-      FACTS: [
-        'Ein Jahr dauert auf dem Merkur nur 88 Tage.',
-        'Die Venus ist zwar weiter von der Sonne entfernt, hat aber höhere Temperaturen als Merkur.',
-        'Venus dreht sich entgegen dem Uhrzeigersinn, möglicherweise aufgrund eines früheren Zusammenstoßes mit einem Asteroiden.',
-        'Auf dem Mars erscheint die Sonne nur halb so groß wie auf der Erde.',
-        'Die Erde ist der einzige Planet, der nicht nach einem Gott benannt ist.',
-        'Jupiter hat den kürzesten Tag aller Planeten.',
-        'Die Milchstraßengalaxis wird in etwa 5 Milliarden Jahren mit der Andromeda-Galaxis zusammenstoßen.',
-        'Die Sonne macht rund 99,86 % der Masse im Sonnensystem aus.',
-        'Die Sonne ist eine fast perfekte Kugel.',
-        'Eine Sonnenfinsternis kann alle ein bis zwei Jahre eintreten. Sie ist daher ein seltenes Ereignis.',
-        'Der Saturn strahlt zweieinhalb mal mehr Energie in den Weltraum aus als er von der Sonne erhält.',
-        'Die Temperatur in der Sonne kann 15 Millionen Grad Celsius erreichen.',
-        'Der Mond entfernt sich von unserem Planeten etwa 3,8 cm pro Jahr.',
-      ],
-      SKILL_NAME: 'Weltraumwissen auf Deutsch',
-      GET_FACT_MESSAGE: 'Hier sind deine Fakten: ',
-      HELP_MESSAGE: 'Du kannst sagen, „Nenne mir einen Fakt über den Weltraum“, oder du kannst „Beenden“ sagen... Wie kann ich dir helfen?',
-      HELP_REPROMPT: 'Wie kann ich dir helfen?',
-      STOP_MESSAGE: 'Auf Wiedersehen!',
     },
   },
 };
@@ -159,71 +88,62 @@ exports.handler = function (event, context) {
 
 const handlers = {
   'LaunchRequest': function () {
-    this.emit('GetFact');
-  },
-  'GetNewFactIntent': function () {
-    this.emit('GetFact');
+    this.emit('GetLatestSongStubruIntent');
   },
   'GetLatestSongMnmIntent': function () {
     var _this = this;
     var myRequest = {
-      'channel': vrtRadioChannels.mnm,
+      'service': 'playlistOnair',
+      'channel': vrtRadioChannel.mnm,
       'onairType': 'latest', // current, previous, latest.
       'filter': 'song', // song, artist, composer, all.
     };
 
-    httpsVrtGetPlaylistOnair(myRequest, handleVrtPlaylistOnair, _this);
+    httpsGetVrtService(myRequest, handleVrtPlaylistOnair, _this);
   },
   'GetLatestSongStubruIntent': function () {
     var _this = this;
     var myRequest = {
-      'channel': vrtRadioChannels.stubru,
+      'service': 'playlistOnair',
+      'channel': vrtRadioChannel.stubru,
       'onairType': 'latest', // current, previous, latest.
       'filter': 'song', // song, artist, composer, all.
     };
 
-    httpsVrtGetPlaylistOnair(myRequest, handleVrtPlaylistOnair, _this);
+    httpsGetVrtService(myRequest, handleVrtPlaylistOnair, _this);
   },
   'GetLatestSongKlaraIntent': function () {
     var _this = this;
     var myRequest = {
-      'channel': vrtRadioChannels.klara,
+      'service': 'playlistOnair',
+      'channel': vrtRadioChannel.klara,
       'onairType': 'latest', // current, previous, latest.
       'filter': 'song', // song, artist, composer, all.
     };
 
-    httpsVrtGetPlaylistOnair(myRequest, handleVrtPlaylistOnair, _this);
+    httpsGetVrtService(myRequest, handleVrtPlaylistOnair, _this);
   },
   'GetLatestSongRadioOneIntent': function () {
     var _this     = this;
     var myRequest = {
-      'channel': vrtRadioChannels.radio1,
+      'service': 'playlistOnair',
+      'channel': vrtRadioChannel.radio1,
       'onairType': 'latest', // current, previous, latest.
       'filter': 'song', // song, artist, composer, all.
     };
 
-    httpsVrtGetPlaylistOnair(myRequest, handleVrtPlaylistOnair, _this);
+    httpsGetVrtService(myRequest, handleVrtPlaylistOnair, _this);
   },
   'GetLatestSongRadioTwoIntent': function () {
     var _this = this;
     var myRequest = {
-      'channel': vrtRadioChannels.radio2,
+      'service': 'playlistOnair',
+      'channel': vrtRadioChannel.radio2,
       'onairType': 'latest', // current, previous, latest.
       'filter': 'song', // song, artist, composer, all.
     };
 
-    httpsVrtGetPlaylistOnair(myRequest, handleVrtPlaylistOnair, _this);
-  },
-  'GetFact': function () {
-    // Get a random space fact from the space facts list
-    // Use this.t() to get corresponding language data
-    const factArr = this.t('FACTS');
-    const factIndex = Math.floor(Math.random() * factArr.length);
-    const randomFact = factArr[factIndex];
-
-    // Create speech output
-    const speechOutput = this.t('GET_FACT_MESSAGE') + randomFact;
-    this.emit(':tellWithCard', speechOutput, this.t('SKILL_NAME'), randomFact);
+    httpsGetVrtService(myRequest, handleVrtPlaylistOnair, _this);
   },
   'AMAZON.HelpIntent': function () {
     const speechOutput = this.t('HELP_MESSAGE');
@@ -271,23 +191,40 @@ var handleVrtPlaylistOnair = function(onairData, myRequest, handlerObject) {
 
 // 3.3. Helper functions ================================================================================================
 
-function httpsVrtGetPlaylistOnair(myRequest, callback, handlerObject) {
+function httpsGetVrtService(myRequest, callback, handlerObject) {
   var onairData = "";
 
-  // Add filter to request.
-  vrtServicePlaylistOnair.path = vrtServicePlaylistOnair.path + '?channel_code=' + myRequest.channel.code;
+  var service = null;
+  
+  if (typeof myRequest.service != 'undefined' && myRequest.service == 'playlistOnair') {
+    // Add filter to request.
+    vrtService.playlistOnair.path = vrtService.playlistOnair.path + '?channel_code=' + myRequest.channel.code;
 
-  // Handle HTTPS request.
-  var req = https.request(vrtServicePlaylistOnair, function(res) {
-    res.setEncoding('utf8');
-    res.on('data', function(dataChunk) {
-      onairData += dataChunk;
+    // Set service.
+    service = vrtService.playlistOnair;
+  }
+
+  if (service != null) {
+    // Handle HTTPS request.
+    var req = https.request(service, function(res) {
+      res.setEncoding('utf8');
+      res.on('data', function(dataChunk) {
+        onairData += dataChunk;
+      });
+      res.on('end', function() {
+        callback(onairData, myRequest, handlerObject);
+      });
     });
-    res.on('end', function() {
-      callback(onairData, myRequest, handlerObject);
-    });
-  });
-  req.end();
+    req.end();
+  }
+  else {
+    if (typeof myRequest.service == 'undefined') {
+      console.log('ERROR: The VRT service is undefined. See myRequest.service variable in handlers.');
+    }
+    else {
+      console.log('ERROR: The VRT service ' + myRequest.service + ' not exits. See myRequest.service variable in handlers.');
+    }
+  }
 };
 
 function getVrtOnairSong(onairData, requestedOnairType) {
@@ -359,13 +296,28 @@ function getSpeechOutput(myResult) {
   // Prepare speechOutput
   speechOutput += 'On ' + speechVars.reqChannel + '. ';
   if (myResult.req.filter == 'song' || myResult.req.filter == 'all') {
-    speechOutput += 'The ' + speechVars.reqOnairType + ' song is ' + speechVars.title + '. ';
+    if (myResult.req.filter == 'all') {
+      speechOutput += 'The ' + speechVars.reqOnairType + ' song is ' + speechVars.title + ' ';
+    }
+    else {
+      speechOutput += 'The ' + speechVars.reqOnairType + ' song is ' + speechVars.title + ' played by ' + speechVars.artist + '.';
+    }
   }
   if (myResult.req.filter == 'artist' || myResult.req.filter == 'all') {
-    speechOutput += 'The artist of the ' + speechVars.reqOnairType + ' song is ' + speechVars.artist + '. ';
+    if (myResult.req.filter == 'all') {
+      speechOutput += 'played by ' + speechVars.artist + '. ';
+    }
+    else {
+      speechOutput += 'The artist of the ' + speechVars.reqOnairType + ' song is ' + speechVars.artist + '. ';
+    }
   }
   if (myResult.req.filter == 'composer' || myResult.req.filter == 'all') {
-    speechOutput += 'The composer of the ' + speechVars.reqOnairType + ' song is ' + speechVars.composer + '.';
+    if (myResult.req.filter == 'all') {
+      speechOutput += 'And the composer is ' + speechVars.composer + '. ';
+    }
+    else {
+      speechOutput += 'The composer of the ' + speechVars.reqOnairType + ' song is ' + speechVars.composer + '.';
+    }
   }
 
   return speechOutput;
